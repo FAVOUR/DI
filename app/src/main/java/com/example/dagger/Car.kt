9a05bc105498a0858b1ccbcd109d9387b65bf339
FAVOUR <truelyfavoured@gmail.com>
2020-04-11 18:47:48 +0100

    Done with Method injection

--- app/src/main/java/com/example/dagger/Car.kt
+++ app/src/main/java/com/example/dagger/Car.kt
@@ -1,14 +1,20 @@
 package com.example.dagger
 
 import com.example.dagger.engine.model.Engine
+import com.example.dagger.remote.Remote
 import com.example.dagger.wheel.wheel
 import timber.log.Timber
 import javax.inject.Inject
 
-
+//Note that
 class Car @Inject constructor(Engine: Engine, wheel: wheel) {
     var TAG: String = javaClass.simpleName
 
+
+    @Inject
+    fun enableRemote(remote: Remote) {
+        remote.setListener(this)
+    }
 
     fun drive(){
     Timber.tag(TAG).d("Driving.....")
